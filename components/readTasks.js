19ddf7ee49c6874c4894997fe382d6a1138c722f
jocnn <ijann_@outlook.com>
2022-11-21 19:47:44 -0300

    se agrupó las tareas con fechas únicas

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,13 +1,22 @@
 import { createTask } from "./addTask.js"
 import dateElement from "./dateElement.js"
+import { uniqueDates } from "../services/data.js"
 
 export const readTasks = () => {
 	const tasksList = JSON.parse(sessionStorage.getItem("tasks")) || []
 
 	const list = document.querySelector("[data-list]")
+	const dates = uniqueDates(tasksList)
 
-	tasksList.forEach((task) => {
-		list.appendChild(dateElement(task.dateFormat))
-		list.appendChild(createTask(task))
+	dates.forEach(date => {
+		const dateMoment = moment(date, "MMMM Do YYYY")
+		list.appendChild(dateElement(date))
+		tasksList.forEach((task) => {
+			const taskDate = moment(task.dateFormat, "MMMM Do YYYY")
+			const diff = dateMoment.diff(taskDate)
+
+			if (diff === 0) list.appendChild(createTask(task))
+		})
 	})
+	
 }
