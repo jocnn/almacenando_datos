65d29733b3a49ef2aae715f0c4a1632acd234dc2
jocnn <ijann_@outlook.com>
2022-11-21 20:04:33 -0300

    se definió la función de ordenación de fechas

--- services/date.js
+++ services/date.js
@@ -7,3 +7,12 @@ export const uniqueDates = (tasks) => {
 
 	return unique
 }
+
+export const orderDates = (dates) => {
+	return dates.sort((a, b) => {
+		const firstDate = moment(a, "MMMM Do YYYY")
+		const secondDate = moment(b, "MMMM Do YYYY")
+
+		return firstDate - secondDate
+	})
+}
