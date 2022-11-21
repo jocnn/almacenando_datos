cbc8605527efb816dbc7e075d7c30a61e2b76878
jocnn <ijann_@outlook.com>
2022-11-21 12:44:55 -0300

    se importó y se llamó a la función

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import { addTask } from "./components/addTask.js"
+import { readTasks } from "./components/readTasks.js"
 
 const btnAdd = document.querySelector("[data-form-btn-add]")
+
+readTasks()
 
 btnAdd.addEventListener("click", addTask)
